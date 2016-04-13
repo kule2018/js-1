a1cf7c9984b14b4314c6418f80c114c3e9d519fa
soneway <250084439@qq.com>
2016-04-13 12:12:44 +0800

    优化

--- perftest.js
+++ perftest.js
@@ -8,7 +8,7 @@
         fnStr = fnStr.slice(fnStr.indexOf('{') + 1);
         fnStr = fnStr.slice(0, fnStr.length - 1);
 
-        console.log(fnStr + ' 运行' + times + '次时长：' + (end - start).toFixed(2));
+        console.log(fnStr + ' 运行' + times + '次时长：' + (end - start).toFixed(2) + 'ms');
     }
 
     //支持performance
@@ -21,7 +21,7 @@
 
             var getEntriesByName = performance.getEntriesByName || performance.webkitGetEntriesByName || performance.msGetEntriesByName || performance.mozGetEntriesByName;
 
-            window.perfTest = function (fn, times) {
+            return window.perfTest = function (fn, times) {
                 var rand = new Date().valueOf();
 
                 //开始时间
@@ -37,22 +37,20 @@
 
                 logPerfInfo(fn, times, start, end);
             };
-
-            return;
         }
     }
 
     //不支持performance.mark
     window.perfTest = function (fn, times) {
         //开始时间
-        var start = new Date().valueOf();
+        var start = +new Date;
         for (var i = 0; i < times; i++) {
             fn();
         }
         //结束时间
-        var end = new Date().valueOf();
+        var end = +new Date;
 
         logPerfInfo(fn, times, start, end);
     };
 
-})(this);
+})(window);
