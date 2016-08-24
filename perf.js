89083093346c44b04f1eb4b2f0d1fef99d30c460
宋均均 <sjj107294@alibaba-inc.com>
2016-08-24 17:14:37 +0800

    优化

--- perf.js
+++ perf.js
@@ -1,62 +1,73 @@
 ﻿(function (window) {
 
-    var performance = window.performance;
+    var perf = (function () {
 
-    function logPerfInfo(fn, times, start, end, msgEl) {
-        var fnStr = fn + '';
+        //打印函数
+        function logPerfInfo(fn, times, start, end, msgEl) {
+            var fnStr = fn + '';
 
-        fnStr = fnStr.slice(fnStr.indexOf('{') + 1);
-        fnStr = fnStr.slice(0, fnStr.length - 1);
+            fnStr = fnStr.slice(fnStr.indexOf('{') + 1);
+            fnStr = fnStr.slice(0, fnStr.length - 1);
 
-        var logStr = fnStr + ' 运行' + times + '次时长：' + (end - start).toFixed(2) + 'ms';
-        msgEl ? msgEl.textContent += logStr + '\n' : console.log(logStr);
+            var logStr = fnStr + ' 运行' + times + '次时长：' + (end - start).toFixed(2) + 'ms';
+            msgEl ? msgEl.textContent += logStr + '\n' : console.log(logStr);
+        }
+
+        //支持performance
+        var performance = window.performance;
+        if (performance) {
+            var mark = performance.mark || performance.webkitMark || performance.msMark || performance.mozMark;
+
+            //支持performance.mark
+            if (mark) {
+                var getEntriesByName = performance.getEntriesByName || performance.webkitGetEntriesByName || performance.msGetEntriesByName || performance.mozGetEntriesByName;
+
+                return function (fn, times, msgEl) {
+                    //默认运行100万次
+                    times || (times = 999999);
+
+                    var rand = +new Date;
+
+                    //开始时间
+                    mark.call(performance, 'start' + rand);
+                    for (var i = 0; i < times; i++) {
+                        fn();
+                    }
+                    //结束时间
+                    mark.call(performance, 'end' + rand);
+
+                    var start = getEntriesByName.call(performance, 'start' + rand)[0].startTime,
+                        end = getEntriesByName.call(performance, 'end' + rand)[0].startTime;
+
+                    logPerfInfo(fn, times, start, end, msgEl);
+                };
+            }
+        }
+
+        //不支持performance.mark
+        return function (fn, times, msgEl) {
+            //默认运行100万次
+            times || (times = 999999);
+
+            //开始时间
+            var start = +new Date;
+            for (var i = 0; i < times; i++) {
+                fn();
+            }
+            //结束时间
+            var end = +new Date;
+
+            logPerfInfo(fn, times, start, end, msgEl);
+        };
+
+    })();
+
+
+    //CommonJS
+    if (typeof exports === 'object') {
+        return module.exports = perf;
     }
 
-    //支持performance
-    if (performance) {
-        var mark = performance.mark || performance.webkitMark || performance.msMark || performance.mozMark;
-
-        //支持performance.mark
-        if (mark) {
-
-            var getEntriesByName = performance.getEntriesByName || performance.webkitGetEntriesByName || performance.msGetEntriesByName || performance.mozGetEntriesByName;
-
-            return window.perf = function (fn, times, msgEl) {
-                //默认运行100万次
-                times || (times = 999999);
-
-                var rand = +new Date;
-
-                //开始时间
-                mark.call(performance, 'start' + rand);
-                for (var i = 0; i < times; i++) {
-                    fn();
-                }
-                //结束时间
-                mark.call(performance, 'end' + rand);
-
-                var start = getEntriesByName.call(performance, 'start' + rand)[0].startTime,
-                    end = getEntriesByName.call(performance, 'end' + rand)[0].startTime;
-
-                logPerfInfo(fn, times, start, end, msgEl);
-            };
-        }
-    }
-
-    //不支持performance.mark
-    window.perf = function (fn, times, msgEl) {
-        //默认运行100万次
-        times || (times = 999999);
-
-        //开始时间
-        var start = +new Date;
-        for (var i = 0; i < times; i++) {
-            fn();
-        }
-        //结束时间
-        var end = +new Date;
-
-        logPerfInfo(fn, times, start, end, msgEl);
-    };
+    window.perf = perf;
 
 })(window);
