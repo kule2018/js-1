675885e7a0fef494f0da8d8272ee604fb6713e0f
宋均均 <sjj107294@alibaba-inc.com>
2016-09-26 12:32:55 +0800

    cdls

--- cdls.js
+++ cdls.js
@@ -1,7 +1,13 @@
 (function (window) {
 
     var cdls = (function () {
-        var document = window.document, ifrEl,
+        var document = window.document,
+            // message事件key
+            GET_ALL_DATA_KEY = 'getAllData',
+            SET_ITEM_KEY = 'setItem',
+            REMOVE_ITEM_KEY = 'removeItem',
+            CLEAR_KEY = 'clear',
+            // visibilitychange事件相关key
             isNoPrefix = document.hidden !== undefined,
             hidden = isNoPrefix ? 'hidden' : 'webkitHidden',
             visibilitychange = isNoPrefix ? 'visibilitychange' : 'webkitvisibilitychange';
@@ -9,21 +15,34 @@
         return function (opts) {
             opts || (opts = {});
 
-            var cdls = {
+            if (!opts.cdlsUrl) {
+                return console.log('cdlsUrl不能为空');
+            }
+
+            var ifrEl, cdls = {
+                reloadData: reloadData,
+                allData: {},
                 getItem: getItem,
-                refreshLs: refreshLs
+                setItem: setItem,
+                removeItem: removeItem,
+                clear: clear
             };
 
             // 收到信息事件
             window.addEventListener('message', function (evt) {
                 var data = evt.data;
-                if (data.id === 'cdls') {
-                    cdls.lsData = data.data;
+                if (data.id === GET_ALL_DATA_KEY) {
+                    cdls.allData = data.data;
+                    // ready
+                    cdls.ready = true;
+                    // success
+                    var success = cdls.success;
+                    typeof success === 'function' && success(cdls.allData);
                 }
             }, false);
 
-            // 刷新ls函数
-            function refreshLs() {
+            // reload数据函数
+            function reloadData() {
                 if (!ifrEl) {
                     ifrEl = document.createElement('iframe');
                     ifrEl.style.display = 'none';
@@ -33,23 +52,59 @@
             }
 
             // 刷新数据
-            refreshLs();
+            reloadData();
 
             // 获取项函数
             function getItem(key) {
-                return cdls.lsData[key];
+                return cdls.allData[key] || null;
+            }
+
+            // 设置项函数
+            function setItem(key, val) {
+                // 发送更新数据信息
+                ifrEl.contentWindow.postMessage({
+                    id: SET_ITEM_KEY,
+                    key: key,
+                    val: val
+                }, opts.cdlsUrl);
+
+                // 同步allData中数据
+                cdls.allData[key] = val;
+            }
+
+            // 移除项函数
+            function removeItem(key) {
+                // 发送更新数据信息
+                ifrEl.contentWindow.postMessage({
+                    id: REMOVE_ITEM_KEY,
+                    key: key
+                }, opts.cdlsUrl);
+
+                // 同步allData中数据
+                cdls.allData[key] = null;
+            }
+
+            // 删除所有
+            function clear() {
+                // 发送更新数据信息
+                ifrEl.contentWindow.postMessage({
+                    id: CLEAR_KEY
+                }, opts.cdlsUrl);
+
+                // 同步allData中数据
+                cdls.allData = {};
             }
 
             // visibilitychange事件
             document.addEventListener(visibilitychange, function () {
                 // 页面重新显示时刷新ls
-                document[hidden] && refreshLs();
+                document[hidden] && reloadData();
             });
 
             return cdls;
         };
     })();
-    
+
 
     if (typeof exports === 'object') {
         return moduels.exports = cdls;
