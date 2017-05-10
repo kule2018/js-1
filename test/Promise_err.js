bab4ed346ab6f67ed57b02e44e320c75b7a033cf
宋均均 <sjj107294@alibaba-inc.com>
2017-05-10 14:23:56 +0800

    test: test

--- test/Promise_err.js
+++ test/Promise_err.js
@@ -1,13 +1,3 @@
-window.addEventListener('error', function(err) {
-  alert(err.message);
-  console.log(err);
-});
-
-window.addEventListener('unhandledrejection', function(err) {
-  alert(err.message);
-  console.log(err);
-});
-
 setTimeout(function() {
   console.log(aaa);
 }, 0);
