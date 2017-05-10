4832967899ba55193975e25c2c92827b42e77c5f
宋均均 <sjj107294@alibaba-inc.com>
2017-05-10 13:55:29 +0800

    test: test

--- test/Promise_err.js
+++ test/Promise_err.js
@@ -8,13 +8,6 @@ window.addEventListener('unhandledrejection', function(err) {
   console.log(err);
 });
 
-function loadScript(url) {
-  var scriptEl = document.createElement('script');
-  scriptEl.type = 'text/javascript';
-  scriptEl.src = url;
-  scriptEl.async = 'async';
-  scriptEl.crossOrigin = 'anonymous';
-  document.head.appendChild(scriptEl);
-}
-
-loadScript('http://image.uc.cn/s/uae/g/1y/xisspages-old-third-party/1.524a877614ad4fb83532.js');
+setTimeout(function() {
+  console.log(aaa);
+}, 1000);
